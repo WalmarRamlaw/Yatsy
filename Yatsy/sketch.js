1a27234270ffeead1076086f10035ebad1709d20
WalmarRamlaw <walmaranders@gmail.com>
2025-09-19 10:30:20 +0200

    Add files via upload

---
+++ Yatsy/sketch.js
@@ -0,0 +1,49 @@
+Antal_spillere = 2
+let Spiller
+Spillere = []
+Antal_slag = 3
+next = false
+spillernr = 0
+
+
+function setup() {
+  createCanvas(600, 600);
+  Spiller = new spiller()
+    for (let i = 0; i < Antal_spillere; i++) {
+    Spillere[i] = "Spiller " + (i + 1);
+  }
+}
+
+
+function draw() {
+  background(220);
+  Spiller.player()
+  //Laver en firkant der er i midten af canvas, der er 50 pixels stor
+  rect(height/2-25,width/2-25,50)
+  
+  //Man registrer muse positionen og hvis den er inden i firkanten eller parametrene bliver position værdi lavet om til 1.
+  if (mouseX > width/2-25 && mouseX < width /2+25 && mouseY > height/2-25 && mouseY < height / 2+25) {
+    position = 1;} 
+  else{
+    //hvis musen ikke er inde i firkanten bliver værdien lavet om til 0 igen
+    position = 0}
+
+  }
+
+//Laver en funktion mere til når musen bliver trykket
+function mousePressed() {
+  //Hvis position = 1, eller inde i firkanten bliver plusset 1 til antal slag
+  if (position == "1") {
+  Antal_slag--
+  }  
+    if (Antal_slag < 1) {
+    Antal_slag = 3
+    next= true
+    spillernr++
+    if (spillernr >= Antal_spillere){
+      spillernr = 0
+    }
+  } else {
+    next = false
+  }
+}
